486d3eb4e51da90e6b8987c13d0c14afcdca2f65
bonface <bonfaceodhiambo405@gmailcom>
2021-12-24 18:00:31 +0300

    my update_files

---
+++ ageteller.py
@@ -0,0 +1,5 @@
+def ageteller(name,age):
+	name= print(input("what is your name"))
+	age = eval(input("what is your age?"))
+	hundred = (100-age)+2020
+	return  print ("you will be 100 years in " +" "+ str(hundred))
